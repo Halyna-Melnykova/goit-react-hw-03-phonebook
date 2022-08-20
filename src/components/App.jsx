015fa4f8563e6607adc6093da1ed9f14ab3e7276
Halyna Melnykova <104796018+Halyna-Melnykova@users.noreply.github.com>
2022-08-20 12:03:47 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  formSubmitHeandler = ({ name, number }) => {
+    const notUniqeContact = this.state.contacts.find(
+      contact => contact.name === name
+    );
+
+    if (notUniqeContact) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState(({ contacts }) => {
+      return { contacts: [newContact, ...contacts] };
+    });
+  };
+
+  changeFilter = event => {
+    const { value } = event.currentTarget;
+    this.setState({ filter: value });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+  render() {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const filtredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+    return (
+      <>
+        <ContactForm onSubmit={this.formSubmitHeandler} />
+        <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList contacts={filtredContacts} onDelete={this.deleteContact} />
+      </>
+    );
+  }
+}
+
+export default App;
